475bb9a128ee243ae9a3c500483af2f0d6b1ba6b
우아테크캠프_2 <woowatechcamp_2@woowatechcamp2.local>
2018-07-05 17:17:58 +0900

    로또 게임 1차 버전 구현 완료

--- src/main/java/lotto/Match.java
+++ src/main/java/lotto/Match.java
@@ -1,18 +1,12 @@
 package lotto;
 
 public enum Match {
-    THIRD(3, 5000), FOURTH(4, 50000), FIFTH(5, 1500000), SIXTH(6, 200000000);
+    THIRD(5000), FOURTH(50000), FIFTH(1500000), SIXTH(200000000);
 
-    private int matchCount;
     private int price;
 
-    Match(int matchCount, int price) {
-        this.matchCount = matchCount;
+    Match(int price) {
         this.price = price;
-    }
-
-    public int getMatchCount(int matchCount) {
-        return matchCount;
     }
 
     public int getPrice() {
